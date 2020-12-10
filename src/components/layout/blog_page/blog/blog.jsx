eefe31cedfab0cd478afcbee8e213f631f09e386
anuj bhatt <anujbhatt023@gmail.com>
2020-12-10 17:30:30 +0530

    blogs mian and styled again

--- src/components/layout/blog_page/blog/blog.jsx
+++ src/components/layout/blog_page/blog/blog.jsx
@@ -13,18 +13,21 @@ import axios from 'axios';
     componentDidMount=()=>{
         axios.get("https://newsrvices.com/wp-json/wp/v2/posts/"+this.props.match.params["id"]).
         then(res=> this.setState({blog:res.data})
-        
+
         );
     }
 
 
     render() {
+       console.log(this.state.blog);
 
         return (
-            <div>
-                {this.state.blog!==null?<><h1>{this.state.blog.title.rendered}</h1>
-                    <td dangerouslySetInnerHTML={{__html:this.state.blog.content.rendered}} />
-                    </>
+            <div className="blog">
+                {this.state.blog!==null?
+                  <div className="blog__box">
+                      <h1 className="blog__box_title">{this.state.blog.title.rendered}</h1>
+                      <td dangerouslySetInnerHTML={{__html:this.state.blog.content.rendered}} />
+                  </div>
                     :null}
             </div>
         )
